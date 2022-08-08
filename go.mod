504f2c89f667db309e0465ab289f9ad88aefa750
Sabino Ramirez <sabinoramirez017@gmail.com>
2022-08-08 05:04:08 -0700

    first

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/sabino-ramirez/jems
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("jems baby")
+
+  // implement login with spotify functionality
+}
